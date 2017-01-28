f2d99c032352a99a76105ff0af30f38d619bebeb
Jason Walsh <j@wal.sh>
2017-01-28 13:14:05 -0800

    `ng lint`

--- youtube-player/src/app/app.module.ts
+++ youtube-player/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import 'hammerjs';
-import { Routes, RouterModule } from '@angular/router'
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -26,7 +26,7 @@ export const routes: Routes = [
         component: NullComponent
     }
 
-]
+];
 
 
 @NgModule({
